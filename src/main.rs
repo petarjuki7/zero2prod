95495c63823c39c2fab87a726b7ab5446df45ffb
petarjuki7 <petar.jukic7@gmail.com>
2024-01-10 19:08:54 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 //! main.rs
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,9 @@
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
