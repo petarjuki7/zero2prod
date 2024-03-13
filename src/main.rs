981dc2671c9f93d88d7500a15a0f7413a4352bc3
petarjuki7 <petar.jukic7@gmail.com>
2024-03-13 12:52:59 +0100

    Added logging and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 //! main.rs
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let subscriber = get_subscriber("zero2prod".to_string(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
